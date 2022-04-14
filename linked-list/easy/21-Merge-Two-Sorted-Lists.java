571a2e3be41dd55d93b52c4661aafeef12edf708
helderzack <helderzacarias@outlook.com>
2022-04-14 20:21:27 -0300

    Smart solution for the Merge Two Sorted LIsts problem; The previious solution was way too long, and way too dumb.

--- linked-list/easy/21-Merge-Two-Sorted-Lists.java
+++ linked-list/easy/21-Merge-Two-Sorted-Lists.java
@@ -10,48 +10,28 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1 == null && list2 == null) return null;
-        ListNode current = new ListNode();
-        ListNode merged = current;
-        ListNode rest = null;
+        ListNode dummy = new ListNode();
+        ListNode tail = dummy;
 
-        while(list1 != null || list2 != null) {
-            if(list1 == null)  {
-                rest = list2;
-                break;
-            }
-
-            if(list2 == null)  {
-                rest = list1;
-                break;
-            }
-
+        while(list1 != null && list2 != null) {
             if(list1.val <= list2.val) {
-                current.val = list1.val;
+                tail.next = list1;
                 list1 = list1.next;
             } else {
-                current.val = list2.val;
+                tail.next = list2;
                 list2 = list2.next;
             }
 
-            current.next = new ListNode();
-            current = current.next;
-            current.next = null;
+            tail = tail.next;
         }
 
-        while(rest != null) {
-            current.val = rest.val;
-            rest = rest.next;
-            if(rest == null) {
-                current.next = null;
-                break;
-            }
-            current.next = new ListNode();
-            current = current.next;
-            current.next = null;
+        if(list1 != null)  {
+            tail.next = list1;
+            return dummy.next;
         }
 
-        return merged;
+        tail.next = list2;
+        return dummy.next;
     }
 
 }
